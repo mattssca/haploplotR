ee0cf2c3bed3452799ed18ce25d0ea62f9e55e13
Carl-Adam Mattsson <mattsada@gmail.com>
2020-11-23 09:19:31 -0800

    Update xy_haploplot.sh

--- scripts/xy_haploplot.sh
+++ scripts/xy_haploplot.sh
@@ -3,4 +3,4 @@
 Rscript ./scripts/xy_haploplot.r
 sh ./scripts/xy_im.crop.sh
 perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf male
-out/ideograms/ideogram.pdf
+rm out/ideograms/ideogram.pdf
