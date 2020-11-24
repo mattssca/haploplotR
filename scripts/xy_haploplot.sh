365db0e96a228150da84c6732acf01afc96bece1
Carl-Adam Mattsson <mattsada@gmail.com>
2020-11-23 17:28:20 -0800

    adding bed sub-setting script

--- scripts/xy_haploplot.sh
+++ scripts/xy_haploplot.sh
@@ -2,5 +2,7 @@
 
 Rscript ./scripts/xy_haploplot.r
 sh ./scripts/xy_im.crop.sh
+Rscript ./scripts/bed_reads_sub.r
+sh ./scripts/bedtools.sub.sh
 perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf male
 rm out/ideograms/ideogram.pdf
