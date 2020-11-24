8a2b2a67e6042f34581d879c1c4b352d4eb96bc6
Carl-Adam Mattsson <mattsada@gmail.com>
2020-11-23 17:27:49 -0800

    adding bed subsetting script

--- scripts/xx_haploplot.sh
+++ scripts/xx_haploplot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 Rscript ./scripts/xx_haploplot.r
+Rscript ./scripts/bed_reads_sub.r
+sh ./scripts/bedtools.sub.sh
 sh ./scripts/xx_im.crop.sh
 perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf female
 rm out/ideograms/ideogram.pdf
