e99bd0eaac301c3a26d16ca25bd38a40f93755a1
vincent hanlon <vhanlon@bccrc.ca>
2020-11-24 16:25:26 +0000

    intermediate commit

--- scripts/xy_haploplot.sh
+++ scripts/xy_haploplot.sh
@@ -4,5 +4,15 @@ Rscript ./scripts/xy_haploplot.r
 sh ./scripts/xy_im.crop.sh
 Rscript ./scripts/bed_reads_sub.r
 sh ./scripts/bedtools.sub.sh
-perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf male
+$hgsid=(perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf male)
 rm out/ideograms/ideogram.pdf
+
+
+
+for bed in $(ls ./in/reads_bed/*.bed ./in/reads_bed/*.bed.gz)
+do
+
+perl ./scripts/pdfplot/scripts/upload.pl "$bed" "$hgsid"
+
+done
+
--- xx_haploplot.sh
+++ xx_haploplot.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 Rscript ./scripts/xx_haploplot.r
-Rscript ./scripts/bed_reads_sub.r
-sh ./scripts/bedtools.sub.sh
 sh ./scripts/xx_im.crop.sh
 hgsid=$(perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf female)
 rm out/ideograms/ideogram.pdf
-
 
 
 for bed in $(ls ./in/reads_bed/*.bed ./in/reads_bed/*.bed.gz)
@@ -15,4 +12,3 @@ do
 perl ./scripts/pdfplot/scripts/upload.pl "$bed" "$hgsid"
 
 done
-
