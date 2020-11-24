e79aeb472e9759a07137841e90b53fc0eaf1b0a1
Carl-Adam Mattsson <mattsada@gmail.com>
2020-11-24 12:58:33 -0800

    Update xx_haploplot.sh

--- scripts/xx_haploplot.sh
+++ scripts/xx_haploplot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Rscript ./scripts/xx_haploplot.r
-Rscript ./scripts/bed_reads_sub.r
+Rscript ./scripts/xx_bed_reads_sub.R
 sh ./scripts/bedtools.sub.sh
 sh ./scripts/xx_im.crop.sh
 perl ./scripts/pdfplot/scripts/urls2pdf_hgsid.pl ./out/bed/*.bed ./out/ideograms/ideogram.pdf female
